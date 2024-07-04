342db3e9319e832f5375ddabe9a5c80e2d6ce164
kauevestena <kauemv2@gmail.com>
2024-07-04 15:01:23 -0300

    more generous timeouts

--- lib.py
+++ lib.py
@@ -142,7 +142,7 @@ def listdir_fullpath(inputfolderpath,extension=None):
             return [os.path.join(inputfolderpath,filename) for filename in os.listdir(inputfolderpath)]
 
 @retry(wait=wait_exponential(multiplier=1, min=10, max=120) + wait_random(min=1, max=12)) # ITS LAZY BUT RESILIENT!!!
-def geojsonl_lazy_dumper(layername,use_alt=False,outfolderpath=None,out_crs=None,chunksize=1000,page_size=100,extra_parameters=None):
+def geojsonl_lazy_dumper(layername,use_alt=False,outfolderpath=None,out_crs=None,chunksize=1000,page_size=100,extra_parameters=None,timeout=None):
     """
     Dumps the features of a given layer in the geojsonl format with resume capabilities.
 
@@ -191,7 +191,7 @@ def geojsonl_lazy_dumper(layername,use_alt=False,outfolderpath=None,out_crs=None
     layer_url , _ , total_feats, layer_metadata = get_basic_layer_stuff(layername,use_alt=use_alt)
 
     # for proper resuming capabilities: 
-    d = EsriDumper(layer_url,start_with=start_idx,max_page_size=page_size,extra_query_args=extra_parameters)
+    d = EsriDumper(layer_url,start_with=start_idx,max_page_size=page_size,extra_query_args=extra_parameters,timeout=timeout)
 
     j = n_chunks # - 1
     outpath = layer_outpath(layername,outfolderpath,j=j)
--- scripts/lazy_dumper_buildings.py
+++ scripts/lazy_dumper_buildings.py
@@ -1,3 +1,6 @@
 from importer import *
 
-geojsonl_lazy_dumper('buildings',use_alt=True,outfolderpath='outputs/buildings',chunksize=350)
+geojsonl_lazy_dumper('buildings',use_alt=True,outfolderpath='outputs/buildings',chunksize=350,
+                    #  extra_parameters={'units':'esriSRUnit_Meter'}
+                    timeout=600
+                     )
